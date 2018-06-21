dc7260ca8ae87b6f36e44802b77fbd1364b4dbdf
Beerscheba C <designergo@aol.com>
2018-06-21 15:45:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,14 +21,3 @@ return array
 }
 
 
-function forLoop(array){
- for( let i = 1; i <25; i++) 
-   
-   if( i ==1){
-   
-   console.log('"I am 1 strange loop."')
- }
- else (console.log("I am ${i} strange loops."))
- 
-return array
-}
