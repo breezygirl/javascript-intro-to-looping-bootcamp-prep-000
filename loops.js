e2d7567cb0a9101381c1d391289b59f549a9ea57
Beerscheba C <designergo@aol.com>
2018-06-21 16:22:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,7 +12,7 @@ return ('done');
 function doWhileLoop(array){
   function maybeTrue() {
     return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
-  }
+
   do {
      array.pop()
   } while ( array.length > 0 && maybeTrue())
